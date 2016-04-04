7c5bd8a9ce1386adaa2d06402e8aff67fb113558
scottcmiller <scottc.miller@live.com>
2016-04-03 21:42:39 -0700

    fixed updateURL

--- track.user.js
+++ track.user.js
@@ -5,7 +5,7 @@
 // @description  Gathers cool robin stats!
 // @author       /u/wave100
 // @match        https://www.reddit.com/robin/
-// @updateURL    https://github.com/wave100/RobinTracker/raw/master/robin.user.js
+// @updateURL    https://github.com/wave100/RobinTracker/raw/master/track.user.js
 // @grant        none
 // ==/UserScript==
 
